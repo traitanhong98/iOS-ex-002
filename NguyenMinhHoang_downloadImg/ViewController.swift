dd40f85dfd13c5ddb04eeedb2a090d234d521242
HoangNM <hoangnm@ttc-solutions.com>
2021-03-09 15:33:28 +0700

    Initial Commit

---
+++ NguyenMinhHoang_downloadImg/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NguyenMinhHoang_downloadImg
+//
+//  Created by Hoang on 3/9/21.
+//  Copyright © 2021 Hoang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
