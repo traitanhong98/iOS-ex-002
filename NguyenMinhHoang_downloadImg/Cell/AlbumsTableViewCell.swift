a6fea0daa38068b25eecab3b4a021c08192ac010
HoangNM <hoangnm@ttc-solutions.com>
2021-03-15 13:45:55 +0700

    hello

---
+++ NguyenMinhHoang_downloadImg/Cell/AlbumsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AlbumsTableViewCell.swift
+//  NguyenMinhHoang_downloadImg
+//
+//  Created by Hoang on 3/15/21.
+//  Copyright © 2021 Hoang. All rights reserved.
+//
+
+import UIKit
+
+class AlbumsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ NguyenMinhHoang_downloadImg/Model/CustomOperation.swift
@@ -0,0 +1,9 @@
+//
+//  CustomOperation.swift
+//  NguyenMinhHoang_downloadImg
+//
+//  Created by Hoang on 3/15/21.
+//  Copyright © 2021 Hoang. All rights reserved.
+//
+
+import Foundation
